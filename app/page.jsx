7ab6df39258b6979da8329c78ec0f47ddd0c4792
Hudson <hudsonvini26@gmail.com>
2024-03-10 13:32:00 -0300

    Add Fonts

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,17 @@
 import styles from "./page.module.css";
+import { Montserrat_Alternates } from "next/font/google"
+
+export const mont_Alt = Montserrat_Alternates({ 
+  subsets: ['latin'],
+  weight: ['400' , '500', '600', '700'] , 
+});
 
 // Componets
 import InitialBanner from "./components/InitialBannerComponent/InitialBanner";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={mont_Alt.className}>
         <InitialBanner />
     </main>
   );
