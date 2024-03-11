5c3f8143a35ba134707537f430dc39089ba94253
Hudson <hudsonvini26@gmail.com>
2024-03-11 00:37:55 -0300

    Add Ux Expirience on hover Card Videos

--- app/components/CardsVideo/CardsVideo.jsx
+++ app/components/CardsVideo/CardsVideo.jsx
@@ -1,11 +1,11 @@
 import style from './CadsVideo.module.css'
 
-export default function CardsVideo() {
+export default function CardsVideo({urlImage}) {
     return(
         <article className={style.cardVideo}>
             {/* Video Area */}
             <div className={style.videoArea}>
-                <img src="https://plus.unsplash.com/premium_photo-1661964014750-963a28aeddea?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Imagem Display" />
+                <img src={urlImage} alt="Imagem Display" />
 
                 <button className={style.buttonPlay}>
                     <svg width="15" height="18" viewBox="0 0 15 18" fill="none" xmlns="http://www.w3.org/2000/svg">
--- app/components/ContainerCards/ContainerCards.jsx
+++ app/components/ContainerCards/ContainerCards.jsx
@@ -10,11 +10,21 @@ export default function ContainerCards() {
             <h2 className={style.sectionTile}>nossos modelos</h2>
 
             <div className={style.containerCards}>
-                <CardsVideo />
-                <CardsVideo />
-                <CardsVideo />
-                <CardsVideo />
-                <CardsVideo />
+                <CardsVideo 
+                    urlImage={'https://plus.unsplash.com/premium_photo-1661964014750-963a28aeddea?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
+                />
+                <CardsVideo 
+                    urlImage={'https://images.unsplash.com/photo-1531971589569-0d9370cbe1e5?q=80&w=2081&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
+                />
+                <CardsVideo 
+                    urlImage={'https://images.unsplash.com/photo-1613082294483-fec382d8367e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
+                />
+                <CardsVideo 
+                    urlImage={'https://images.unsplash.com/photo-1611095210561-67f0832b1ca3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
+                />
+                <CardsVideo 
+                    urlImage={'https://images.unsplash.com/photo-1578683010236-d716f9a3f461?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
+                />
             </div>
 
         </section>
